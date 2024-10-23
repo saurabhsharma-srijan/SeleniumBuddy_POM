ada341e7fea24c89695e11ab3e3eee7d53fa76b0
shubham_gupta_materialplus <shubham.gupta@materialplus.io>
2024-10-23 21:46:18 +0530

    Added Options feature in browser execution

--- src/test/java/org/example/BaseTest.java
+++ src/test/java/org/example/BaseTest.java
@@ -1,9 +1,11 @@
 package org.example;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.*;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 
 public class BaseTest {
     WebDriver driver;
@@ -11,8 +13,23 @@ public class BaseTest {
     @BeforeTest
     public void setUp() {
         WebDriverManager.firefoxdriver().setup();
-        driver = new FirefoxDriver();
+
+        // Define FirefoxOptions to add capabilities
+        FirefoxOptions options = new FirefoxOptions();
+
+        // Example: Adding capabilities
+        options.addPreference("browser.download.folderList", 2);  // Set custom download folder
+        options.addPreference("browser.download.dir", "/path/to/download"); // Set download directory
+        options.addPreference("browser.helperApps.neverAsk.saveToDisk", "application/pdf"); // Skip save-to-disk prompt for PDFs
+        options.setHeadless(true);  // Enable or disable headless mode
+
+        // Initialize FirefoxDriver with options
+        driver = new FirefoxDriver(options);
+
+        // Maximize the window
         driver.manage().window().maximize();
+
+        // Example: Navigating to a URL
         driver.get("https://automationteststore.com/");
     }
 
