61e49472f9a0746290761ae4bad0a53062244d63
shubham_gupta_materialplus <shubham.gupta@materialplus.io>
2024-10-23 22:47:03 +0530

    Added Testng.xml file

--- src/test/java/org/example/BaseTest.java
+++ src/test/java/org/example/BaseTest.java
@@ -1,42 +1,56 @@
 package org.example;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.*;
 
 public class BaseTest {
     WebDriver driver;
+    public ExtentReports extent;
+    public ExtentTest test;
 
-    @BeforeTest
+    @BeforeSuite
+    public void setupExtent() {
+        ExtentSparkReporter spark = new ExtentSparkReporter("target/ExtentReport.html");
+        extent = new ExtentReports();
+        extent.attachReporter(spark);
+    }
+
+
+
+
+
+    @BeforeMethod
     public void setUp() {
-        WebDriverManager.firefoxdriver().setup();
 
-        // Define FirefoxOptions to add capabilities
-        FirefoxOptions options = new FirefoxOptions();
+        WebDriverManager.chromedriver().setup();
+        // Create an instance of ChromeDriver
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");// For opening the browser in headless mode
+        options.addArguments("--disable-gpu"); // Optional, for specific configurations
+        driver = new ChromeDriver(options);
 
-        // Example: Adding capabilities
-        options.addPreference("browser.download.folderList", 2);  // Set custom download folder
-        options.addPreference("browser.download.dir", "/path/to/download"); // Set download directory
-        options.addPreference("browser.helperApps.neverAsk.saveToDisk", "application/pdf"); // Skip save-to-disk prompt for PDFs
-        options.setHeadless(true);  // Enable or disable headless mode
-
-        // Initialize FirefoxDriver with options
-        driver = new FirefoxDriver(options);
-
-        // Maximize the window
+        // Maximize the browser window
         driver.manage().window().maximize();
 
         // Example: Navigating to a URL
         driver.get("https://automationteststore.com/");
     }
 
-    @AfterTest
+    @AfterMethod
     public void tearDown() {
         if (driver != null) {
             driver.quit();
         }
+    }
+
+    @AfterSuite
+    public void tearDownExtent() {
+        extent.flush(); // Make sure to flush to write results to the report
     }
 }
